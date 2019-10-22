18226666ba94cdc4df4fce586e34a6d97ec3c27a
Chris Dunstan <dunstanc@wlhs.wlwv.k12.or.us>
2019-10-22 00:15:19 -0700

    edited Star Printer at home

--- StarPrinter.java
+++ StarPrinter.java
@@ -11,7 +11,7 @@ public class StarPrinter{
     boolean valid = false;
     while(!valid){
       System.out.println("");
-      Sytem.out.pritnln("Give a number 1-5 to get your lucky symbol. ");
+      System.out.pritnln("Give a number 1-5 to get your lucky symbol. ");
       System.out.println("Say 6 for a surprise. ");
       System.out.println("");
       System.out.print("-> ");
@@ -27,7 +27,7 @@ public class StarPrinter{
           System.out.println("You must input a number between 1 and 6.");
         }
       }
-      catch(java.InputMismatchException e){
+      catch(java.util.InputMismatchException e){
         in.next();
         System.out.println("");
         System.out.println("You must input a whole number between 1 and 6.");
@@ -35,13 +35,14 @@ public class StarPrinter{
     }
 
     int numStar = 0;
+    int numSpace = 0;
 
     if (option == 1){
       System.out.println("");
       for (int i = 0; i < WIDTH; ++i){
         numStar = WIDTH - i;
         System.out.println("");
-        for (int k = 1; k < numStar, ++k){
+        for (int k = 1; k < numStar; ++k){
           System.out.print("*");
         }
       }
@@ -52,11 +53,11 @@ public class StarPrinter{
       for (int i = 0; i < WIDTH; ++i){
         numStar = i;
         System.out.println("");
-        for (int k = 0; k < (WIDTH - numStar), ++k){
+        for (int k = 0; k < (WIDTH - numStar); ++k){
           System.out.print(" ");
         }
-        for (int l = 0; l < numStar, ++l){
-          System.out.print("*")
+        for (int l = 0; l < numStar; ++l){
+          System.out.print("*");
         }
       }
     }
@@ -66,20 +67,46 @@ public class StarPrinter{
       for (int i = 0; i < WIDTH; ++i){
         numStar = WIDTH - i;
         System.out.println("");
-        for (int k = 0; k < numStar, ++k){
+        for (int k = 0; k < numStar; ++k){
           System.out.print("*");
         }
-        for (int l = 0; l < (WIDTH - numStar), ++l){
-          System.out.print(" ")
+        for (int l = 0; l < (WIDTH - numStar); ++l){
+          System.out.print(" ");
         }
       }
     }
 
     else if (option == 4){
-      int halfDim = (WIDTH + 1)/2
+      int halfDim = (WIDTH + 1)/2;
 
-      for (int i = 0; i < halfDim, ++i){
-        
+      for (int i = 0; i < halfDim; ++i){
+            numStar = 2 * i + 1;
+            numSpace = (WIDTH - numStar) / 2;
+
+            System.out.println("");
+
+            for (int k = 0; k < numSpace; ++k) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < numStar; ++k) {
+                System.out.print("*");
+            }
+        }
+
+        for (int i = halfDim; i > 0; --i){
+            numStar = 2*i + 1;
+            numSpace = (WIDTH - numStar)/2;
+
+            System.out.println("");
+
+            for (int k = 0; k < numSpace; ++k){
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k<numStar; ++k) {
+                System.out.print("*");
+            }
       }
     }
 
